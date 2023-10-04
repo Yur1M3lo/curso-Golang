35f8873ffd2f05d308de6be2935eecd2f729cb0b
Yur1M3lo <yurimelo116@gmail.com>
2023-10-03 21:29:40 -0300

    adicionando exemplo de panic e recover

---
+++ exemplos/14-funcoesAvancadas/panicRecover/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func recuperarExecucao() {
+	if r := recover(); r != nil {
+		fmt.Println("Execução recuperada com sucesso!")
+	}
+}
+
+func alunoEstaAprovado(n1, n2 float64) bool {
+	defer recuperarExecucao()
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+
+	panic("MEDIA E IGUAL A SEIS!")
+}
+
+func main() {
+	fmt.Println(alunoEstaAprovado(6, 6))
+	fmt.Println("PROGRAMA FINALIZADO!")
+}
