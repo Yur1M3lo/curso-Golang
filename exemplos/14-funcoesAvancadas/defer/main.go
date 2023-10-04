7645d1c6db91440b58c894dbb3f24547fe25b06d
Yur1M3lo <yurimelo116@gmail.com>
2023-10-03 21:05:44 -0300

    adicionando exemplo com defer

---
+++ exemplos/14-funcoesAvancadas/defer/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func funcao1() {
+	fmt.Println("Executando função 1")
+}
+
+func funcao2() {
+	fmt.Println("Executando função 2")
+}
+
+
+func main() {
+	defer funcao1()
+	funcao2()
+}
