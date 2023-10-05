d82b220894427b3d9f7673ecd138672ed7939b0a
Yur1M3lo <yurimelo116@gmail.com>
2023-10-05 20:29:33 -0300

    adicionando codigo para pegar os endereços ips passados por parametro

--- appLinhaComando/app/app.go
+++ appLinhaComando/app/app.go
@@ -1,12 +1,44 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+	"github.com/urfave/cli"
+)
 
 // Gerar vai retorna a aplicação de linha de comando pronta para ser executada
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e nome de servidores na internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name: "ip",
+			Usage: "Busca Ips de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "Host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: BuscarIps,
+		},
+	}
 	
 	return app
+}
+
+func BuscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
